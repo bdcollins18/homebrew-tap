95d159c65da4d563c74ae72630b907ad1dacc7c0
Brian Collins <bdcollins18@gmail.com>
2020-09-04 01:13:36 -0400

    Moved duplicacy-cli formula from old homebrew-duplicacy-cli repo into new homebrew-tap repo

---
+++ Casks/duplicacy-cli.rb
@@ -0,0 +1,9 @@
+cask 'duplicacy-cli' do
+	name 'duplicacy-cli'
+	version '2.6.1'
+	url "https://github.com/gilbertchen/duplicacy/releases/download/v#{version}/duplicacy_osx_x64_#{version}"
+	sha256 'db08423f54abc19bb8607da93ff7d67c452c19c41140f8d292af3e722ea24a3b'
+	homepage 'https://duplicacy.com/'
+
+	binary "duplicacy_osx_x64_#{version}", target: 'duplicacy'
+end

be4f0163557152fbc81f2f88fe1f0361703362e7
Brian Collins <bdcollins18@gmail.com>
2020-09-06 22:33:30 -0400

    Added uninstall stanza w/ package names

--- Casks/etc-nomad.rb
+++ Casks/etc-nomad.rb
@@ -1,13 +1,15 @@
 cask "etc-nomad" do
-	name "etc-nomad"
 	version "3.0.0.389"
 	sha256 "fc055df5ac09a58b75cd54bbfe3255e9bc3adac2760c78730ea46ed39ab1ccd6"
-	homepage "https://www.etcconnect.com/Products/Consoles/Eos-Family/"
-	url "https://cdn.etcconnect.com/ETCnomad%20Eos%20Mac%20#{version}.zip"
-	pkg "ETCnomad Eos Mac #{version}.pkg",
-		choices: [
-			{
 
-			}
-		]
+	name "ETC-Nomad"
+	desc "The ETC Nomad software allows your computer to act as an ETC Eos or Element lighting console and includes ETC Augment3d visualization"
+	homepage "https://www.etcconnect.com/Products/Consoles/Eos-Family/"
+
+	url "https://cdn.etcconnect.com/ETCnomad%20Eos%20Mac%20#{version}.zip"
+	pkg "ETCnomad Eos Mac #{version}.pkg"
+	uninstalll pkgutil: [
+		"com.ETC.pkg.ETCnomadEos",
+		"com.ETC.pkg.slp"
+	]
 end

876ad53f4718303ec7988f9b64b2a36412abfcd2
Brian Collins <bdcollins18@gmail.com>
2020-09-06 21:56:14 -0400

    Added SHA256 and used real download link

--- Casks/etc-nomad.rb
+++ Casks/etc-nomad.rb
@@ -1,11 +1,9 @@
 cask "etc-nomad" do
 	name "etc-nomad"
 	version "3.0.0.389"
-	download_id = "10737502789"
-	sha256 ""
+	sha256 "fc055df5ac09a58b75cd54bbfe3255e9bc3adac2760c78730ea46ed39ab1ccd6"
 	homepage "https://www.etcconnect.com/Products/Consoles/Eos-Family/"
-
-	url "https://www.etcconnect.com/WorkArea/DownloadAsset.aspx?id=#{download_id}"
+	url "https://cdn.etcconnect.com/ETCnomad%20Eos%20Mac%20#{version}.zip"
 	pkg "ETCnomad Eos Mac #{version}.pkg",
 		choices: [
 			{

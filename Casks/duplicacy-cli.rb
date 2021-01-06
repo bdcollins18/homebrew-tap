b1552f2b9a0d1f792c963ad3580e365f85e98746
Brian Collins <bdcollins18@gmail.com>
2021-01-06 02:48:46 -0500

    Changed duplicacy-cli version to 2.7.2

--- Casks/duplicacy-cli.rb
+++ Casks/duplicacy-cli.rb
@@ -1,6 +1,6 @@
 cask 'duplicacy-cli' do
 	name 'duplicacy-cli'
-	version '2.6.1'
+	version '2.7.2'
 	url "https://github.com/gilbertchen/duplicacy/releases/download/v#{version}/duplicacy_osx_x64_#{version}"
 	sha256 'db08423f54abc19bb8607da93ff7d67c452c19c41140f8d292af3e722ea24a3b'
 	homepage 'https://duplicacy.com/'

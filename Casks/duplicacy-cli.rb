1375f246b8e8bb0c063863e33a959232d919a918
Brian Collins <bdcollins18@gmail.com>
2021-01-06 02:50:25 -0500

    Updated SHA for duplicacy-cli

--- Casks/duplicacy-cli.rb
+++ Casks/duplicacy-cli.rb
@@ -2,7 +2,7 @@ cask 'duplicacy-cli' do
 	name 'duplicacy-cli'
 	version '2.7.2'
 	url "https://github.com/gilbertchen/duplicacy/releases/download/v#{version}/duplicacy_osx_x64_#{version}"
-	sha256 'db08423f54abc19bb8607da93ff7d67c452c19c41140f8d292af3e722ea24a3b'
+	sha256 '7d76c03c965d2d39ca11ef2d6d50bdc3ed0a37e4a2384aed5d8b7a3ee81449f2'
 	homepage 'https://duplicacy.com/'
 
 	binary "duplicacy_osx_x64_#{version}", target: 'duplicacy'
